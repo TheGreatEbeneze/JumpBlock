7b0f38b440cbfa0754d514de03ca55251eb2d039
Jason Hays <jasonhays@jasons-imac.lan>
2025-05-26 19:19:29 -0700

    First commit

---
+++ playscreenback.gd
@@ -0,0 +1,22 @@
+extends Node3D
+@onready var playscreen: Node2D = $playscreen
+@onready var button: Button = $playscreen/Button
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+var  mover = 1
+@onready var timer: Timer = $Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	mover = randf_range(1, 2)
+	
+	playscreen.hide()
+	animation_player.play("camera1")
